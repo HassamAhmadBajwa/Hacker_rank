8d66e29da39e0d03a7248bf566676f190cf65b1a
Hassam Ahmad Bajwa <hassamahmadbajwa@gmail.com>
2024-04-10 03:35:39 +0500

    recode reverse array

--- reverseArray.cpp
+++ reverseArray.cpp
@@ -1,31 +1,33 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 int main()
 {
     // initialize the size of the array
-    int n;
-    cin >> n;
-    int *arr = new int(n);
-    for (int i = 0; i < n; i++)
+    vector<int> vec;
+    int size;
+    cin >> size;
+    int arr;
+    for (int i = 0; i < size; i++)
     {
-        cin >> arr[i];
+        cin >> arr;
+        vec.push_back(arr);
     }
     // reverse the array
     int i = 0;
-    int j = n - 1;
+    int j = size - 1;
     for (int i = 0; i < j; i++)
     {
         int temp;
-        temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
+        temp = vec[i];
+        vec[i] = vec[j];
+        vec[j] = temp;
         j--;
     }
     // print the reverse array
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < size; i++)
     {
-        cout << arr[i] << " ";
+        cout << vec[i] << " ";
     }
     // delete the array from heap;
-    delete[] arr;
 }
