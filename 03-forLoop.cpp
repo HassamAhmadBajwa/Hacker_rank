ed6b51e18e04c49fd56e851b54092472b4e38023
Hassam Ahmad Bajwa <hassamahmadbajwa@gmail.com>
2024-04-10 04:23:29 +0500

    recode for loop

--- 03-forLoop.cpp
+++ 03-forLoop.cpp
@@ -56,37 +56,31 @@
 // odd
 
 #include <iostream>
+#include <vector>
 using namespace std;
 int main()
 {
-    int a, b;
-    string array[] = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
 
-    cin >> a;
-    cin >> b;
-    int length = sizeof(array) / sizeof(array[0]);
+    vector<string> vec;
+    vec = {"one ", "two ", "three ", "four ", "five ", "six ", "seven ", "eight ", "nine "};
+
+    int a, b;
+    cin >> a >> b;
     for (int i = a; i <= b; i++)
     {
-        if (i <= 9 && i >= 1 && i == a)
+        if (i <= 9)
         {
-            cout << array[i - 1] << endl;
+            cout << vec[i - 1] << endl;
         }
-        else if (i <= 9 && i >= 1 && i == b)
+        else if (i % 2 == 0)
         {
-            cout << array[i - 1] << endl;
+            cout << "even" << endl;
         }
-        else if (i > 9)
+        else
         {
-            if (i % 2 == 0)
-            {
-                cout << "even" << endl;
-            }
-            else
-            {
-                cout << "odd" << endl;
-            }
-
-            break;
+            cout << "odd" << endl;
         }
     }
+
+    return 0;
 }
