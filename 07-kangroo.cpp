546ceff00ada6be342b32882bcd45cf9069a29d4
Hassam Ahmad Bajwa <hassamahmadbajwa@gmail.com>
2024-04-16 03:20:17 +0500

    add 07-kangroo problem

---
+++ 07-kangroo.cpp
@@ -0,0 +1,28 @@
+// Hacker Rank kangroo problem solving
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x1, v1, x2, v2;
+    string s1 = "YES";
+    string s2 = "NO";
+    cout << "Enter the value of x1, v1, x2, v2:: ";
+    cin >> x1 >> v1 >> x2 >> v2;
+    for (int i = 1; i <= x2; i++)
+    {
+        if (x1 >= 0 && x2 > x1 && v1 >= 1 && v2 >= 1)
+        {
+            x1 = x1 + (v1 + 1);
+            x2 = x2 + (v2 + 1);
+        }
+    }
+    if (x1 == x2)
+    {
+        cout << s1 << endl;
+    }
+    else
+    {
+        cout << s2 << endl;
+    }
+}
